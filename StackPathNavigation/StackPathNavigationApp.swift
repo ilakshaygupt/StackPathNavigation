0785a7f331b6a09c275b286bc553cc08f94191b1
lakshay <gupta22154006@akgec.ac.in>
2024-11-07 15:36:25 +0530

    Initial Commit

---
+++ StackPathNavigation/StackPathNavigationApp.swift
@@ -0,0 +1,17 @@
+//
+//  StackPathNavigationApp.swift
+//  StackPathNavigation
+//
+//  Created by Lakshay Gupta on 07/11/24.
+//
+
+import SwiftUI
+
+@main
+struct StackPathNavigationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
